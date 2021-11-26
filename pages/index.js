5552b1e4ccff63e967bedcedff5da7ee5373f1e0
Paritosh Srivastava <srivastavaparitosh1000@gmail.com>
2021-11-26 21:02:26 +0530

    Added cards of theme

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
 import styles from '../styles/Home.module.css'
 import NavBar from '../components/NavBar'
+import Theme from '../components/Theme'
 
 export default function Home() {
   return (
+    <>
     <div className={styles.container}>
       <NavBar></NavBar>
     </div>
+    <div className={styles.container}>
+      <Theme></Theme>
+    </div>
+    </>
   )
 }
