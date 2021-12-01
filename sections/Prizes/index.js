493fa104c75df0b589c139880042a37612d33ef0
Anubha13kumari <anubhamuz@gmail.com>
2021-12-01 11:41:38 +0530

    prize section

--- sections/Prizes/index.js
+++ sections/Prizes/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import Image from 'next/image'
-import prize0 from '../../public/prize0.png'
-import prize6 from '../../public/prize6.png'
-import prize5 from '../../public/prize5.png'
+import prize1 from '../../public/prize1.png'
+import prize2 from '../../public/prize2.png'
+import prize3 from '../../public/prize3.png'
 import prize4 from '../../public/prize4.png'
 import { Card } from 'react-bootstrap'
 import styles from '../../styles/Prize.module.css'
@@ -15,7 +15,7 @@ export default function Prize(){
    <div className="section">
    <div className="row">
         <Card className={styles.Card}style={{ width: '18rem' }} >
-        <Image variant="top" src={ prize4 } className="img-card" width={200} height={200} style={{ margin: 'auto' }} />
+        <Image variant="top" src={ prize1 } className="img-card" width={100} height={100}  />
   <Card.Body>
     <Card.Title >1st Prize!!!</Card.Title>
     <Card.Text>
@@ -25,7 +25,7 @@ export default function Prize(){
 </Card>
 
 <Card className={styles.Card} style={{ width: '18rem' }}>
-<Image variant="top" src={ prize5 } className="img-card" width={200} height={200} style={{ margin: 'auto' }} />
+<Image variant="top" src={ prize2 } className="img-card" width={100} height={100}  />
   <Card.Body>
     <Card.Title>2nd Prize!!!</Card.Title>
     <Card.Text>
@@ -35,7 +35,7 @@ export default function Prize(){
 </Card>
 
 <Card className={styles.Card} style={{ width: '18rem' }}>
-<Image variant="top" src={ prize6 } className="img-card" width={200} height={200} />
+<Image variant="top" src={ prize3 } className="img-card" width={100} height={100}  />
   <Card.Body>
     <Card.Title>3rd Prize!!!</Card.Title>
     <Card.Text>
@@ -45,7 +45,7 @@ export default function Prize(){
 </Card>
 
 <Card  className={styles.Card} style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="holder.js/100px180" />
+<Image variant="top" src={ prize4 } className="img-card" width={100} height={100}  />
   <Card.Body>
     <Card.Title>Other Excting Prizes for all Participants</Card.Title>
     <Card.Text>
