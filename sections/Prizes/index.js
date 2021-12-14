bbb2a94794f2781feddb137b6153614752e0a89f
Rishabhraghwendra18 <rishabhraghwendra2002@gmail.com>
2021-12-14 15:58:39 +0530

    husky added

--- sections/Prizes/index.js
+++ sections/Prizes/index.js
@@ -11,50 +11,8 @@ import styles from '../../styles/Prize.module.css'
 
 export default function Prize(){
     return (
- <div className="d-flex justify-content-center">
-   <div className="section">
-   <div className="row">
-        <Card className={styles.Card}style={{ width: '18rem' }} >
-        <Image variant="top" src={ prize1 } className="img-card" width={100} height={100}  />
-  <Card.Body>
-    <Card.Title >1st Prize!!!</Card.Title>
-    <Card.Text>
-    The team which builds the best hack
-    </Card.Text>
-    </Card.Body>
-</Card>
-
-<Card className={styles.Card} style={{ width: '18rem' }}>
-<Image variant="top" src={ prize2 } className="img-card" width={100} height={100}  />
-  <Card.Body>
-    <Card.Title>2nd Prize!!!</Card.Title>
-    <Card.Text>
-    The team which builds the 2nd best hack
-    </Card.Text>
-    </Card.Body>
-</Card>
-
-<Card className={styles.Card} style={{ width: '18rem' }}>
-<Image variant="top" src={ prize3 } className="img-card" width={100} height={100}  />
-  <Card.Body>
-    <Card.Title>3rd Prize!!!</Card.Title>
-    <Card.Text>
-    The team which builds the 2nd best hack
-    </Card.Text>
-    </Card.Body>
-</Card>
-
-<Card  className={styles.Card} style={{ width: '18rem' }}>
-<Image variant="top" src={ prize4 } className="img-card" width={100} height={100}  />
-  <Card.Body>
-    <Card.Title>Other Excting Prizes for all Participants</Card.Title>
-    <Card.Text>
-    Other Excting Prizes for all Participants
-    </Card.Text>
-    </Card.Body>
-</Card>
-</div>
-</div>
+ <div className="d-flex justify-content-center flex-row">
+        
 </div>
 
     )
