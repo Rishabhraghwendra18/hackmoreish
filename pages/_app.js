e363a70ae162c17539650a295973070f7a83920a
Paritosh Srivastava <srivastavaparitosh1000@gmail.com>
2021-12-17 23:34:38 +0530

    added footer section

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/globals.css";
 import NavBar from '../components/NavBar'
+import Footer from "../sections/Footer";
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -10,9 +11,11 @@ function MyApp({ Component, pageProps }) {
         <title>HackMoreish</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
       </Head>
       <NavBar></NavBar>
       <Component {...pageProps} />
+      <Footer></Footer>
     </>
   );
 }
---
+++ sections/Footer/index.js
@@ -0,0 +1,26 @@
+
+
+
+export default function Footer() {
+    return(
+       
+<div>
+
+  <footer className="text-center text-lg-start" style={{backgroundColor:" #000;"}}>
+    <div className="container d-flex justify-content-center py-4">
+      
+        <i className="fab fa-twitter" style={{fontSize: "24px", padding: "20px;"}}></i>
+        <i className="fab fa-facebook" style={{fontSize: "24px", padding: "20px;"}}></i>
+        <i className="fab fa-instagram" style={{fontSize: "24px", padding: "20px;"}}></i>
+      
+    </div>
+     <div className="text-center text-white p-3">
+      © 2020 Copyright: HackMoreish
+    </div>
+    
+  </footer>
+  
+</div>
+
+    );
+    }
