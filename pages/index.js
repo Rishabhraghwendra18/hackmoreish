18f2e1ba0f1132165da6d89697176fad71f1da30
Paritosh Srivastava <srivastavaparitosh1000@gmail.com>
2021-12-18 13:20:11 +0530

    added footer section

--- pages/index.js
+++ pages/index.js
@@ -4,50 +4,50 @@ import Prizes from '../sections/Prizes/index'
 import Theme from '../sections/Theme/index'
 import About from '../sections/About/index'
 
-// export async function getStaticProps() {
-//   const query = `{
-//     nameCollection{
-//       items{
-//         title
-//         description
-//         icon{
-//           description
-//           url
-//           width
-//           height
-//         }
-//       }
-//     }
-//     prizesCollection{
-//       items{
-//         title
-//         description
-//         icon{
-//           description
-//           url
-//           width
-//           height
-//         }
-//       }
-//     }
-//   }`;
-//   const response = await fetch(`https://graphql.contentful.com/content/v1/spaces/${process.env.SPACE_ID}/environments/master`, {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//       Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
-//     },
-//     body: JSON.stringify({ query })
-//   }
-//   )
-//     .then(res => res.json());
-//   return {
-//     props: {
-//       content: response.data.nameCollection.items,
-//       prizes: response.data.prizesCollection.items
-//     }
-//   };
-// }
+export async function getStaticProps() {
+  const query = `{
+    nameCollection{
+      items{
+        title
+        description
+        icon{
+          description
+          url
+          width
+          height
+        }
+      }
+    }
+    prizesCollection{
+      items{
+        title
+        description
+        icon{
+          description
+          url
+          width
+          height
+        }
+      }
+    }
+  }`;
+  const response = await fetch(`https://graphql.contentful.com/content/v1/spaces/${process.env.SPACE_ID}/environments/master`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
+    },
+    body: JSON.stringify({ query })
+  }
+  )
+    .then(res => res.json());
+  return {
+    props: {
+      content: response.data.nameCollection.items,
+      prizes: response.data.prizesCollection.items
+    }
+  };
+}
 
 export default function Home({ content, prizes }) {
   return (
@@ -55,8 +55,8 @@ export default function Home({ content, prizes }) {
       <div className={styles.container}>
         <Hero></Hero>
         <About></About>
-        {/* <Prizes prizes={prizes}></Prizes>
-        <Theme content={content}></Theme> */}
+        <Prizes prizes={prizes}></Prizes>
+        <Theme content={content}></Theme>
       </div>
     </>
   )
