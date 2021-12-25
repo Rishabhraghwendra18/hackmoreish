4d01f6b51776640b5075cd84283556210fca0070
Rishabhraghwendra18 <rishabhraghwendra2002@gmail.com>
2021-12-25 15:01:27 +0530

    changed size of title in Div to 5vw

--- components/Div.js
+++ components/Div.js
@@ -4,7 +4,7 @@ export default function Div({title=null,section_id,children}) {
   return (
     <div className={`${styles.box} vh-100 d-flex justify-content-center `} id={section_id}>
       <div className={`d-flex justify-content-center flex-column ${styles.heading}`}>
-            {title ?  <h1 style={{textAlign:"center",fontSize:"6vw !important"}}>{title}</h1> : null}
+            {title ?  <h1 style={{textAlign:"center",fontSize:"5vw !important"}}>{title}</h1> : null}
             {children}
       </div>
     </div>
